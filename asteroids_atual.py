4afb3c676aac04f4b202d26350cc12b49d31130d
ceciliaprado <ceciliadmp@al.insper.edu.br>
2021-08-11 16:33:47 -0300

    movimentacao lateral

--- asteroids_atual.py
+++ asteroids_atual.py
@@ -99,6 +99,21 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
+        # Verifica se apertou alguma tecla.
+        if event.type == pygame.KEYDOWN:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player.speedx -= 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx += 8
+        # Verifica se soltou alguma tecla.
+        if event.type == pygame.KEYUP:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player.speedx += 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx -= 8
+
     # ----- Atualiza estado do jogo
     # Atualizando a posição dos meteoros
     all_sprites.update()
