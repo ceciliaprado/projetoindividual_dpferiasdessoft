428a027a25633446e1088001c4391b5991e114b7
ceciliaprado <ceciliadmp@al.insper.edu.br>
2021-08-11 15:25:11 -0300

    movimento aleatório dos meteoros, arrumando velocidade e tamanho

--- asteroids_atual.py
+++ asteroids_atual.py
@@ -20,20 +20,47 @@ background = pygame.image.load('assets/starfield2.jpg').convert()
 meteor_img = pygame.image.load('assets/coroninha.png').convert_alpha()
 meteor_img_small = pygame.transform.scale(meteor_img, (METEOR_WIDTH, METEOR_HEIGHT))
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+# Definindo os novos tipos
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, WIDTH-METEOR_WIDTH)
+        self.rect.y = random.randint(-100, - METEOR_HEIGHT)
+        self.speedx = random.randint(-3, 3)
+        self.speedy = random.randint(2, 9)
+
+    def update(self):
+        # Atualizando a posição do meteoro
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        # Se o meteoro passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.rect.x = random.randint(0, WIDTH-METEOR_WIDTH)
+            self.rect.y = random.randint(-100, -METEOR_HEIGHT)
+            self.speedx = random.randint(-3, 3)
+            self.speedy = random.randint(2, 9)
+
+
 # ----- Inicia estruturas de dados
 game = True
-# Sorteia posição aleatória
-# Como x é o lado esquerdo da imagem, ele só pode ir até a largura da
-# janela menos a largura da imagem
-meteor_x = random.randint(0, WIDTH-METEOR_WIDTH)
-# y negativo significa que está acima do topo da janela. O meteoro começa fora da janela
-meteor_y = random.randint(-100, -METEOR_HEIGHT)
-# Sorteia velocidade do meteoro
-meteor_speedx = random.randint(-3, 3)
-meteor_speedy = random.randint(2, 9)
-# Variável para o ajuste de velocidade
+
+# Criando um grupo de meteoros
+all_meteors = pygame.sprite.Group()
+# Criando os meteoros
+for i in range(8):
+    meteor = Meteor(meteor_img)
+    all_meteors.add(meteor)
+
+    
+#velocidade
 clock = pygame.time.Clock()
-FPS = 15
+FPS = 30
 
 # ===== Loop principal =====
 while game:
@@ -45,21 +72,19 @@ while game:
             game = False
 
     # ----- Atualiza estado do jogo
-    # Atualizando a posição do meteoro
-    meteor_x += meteor_speedx
-    meteor_y += meteor_speedy
-    # Se o meteoro passar do final da tela, volta para cima
-    # novas posições e velocidades
-    if meteor_y > HEIGHT or meteor_x + METEOR_WIDTH < 0 or meteor_x > WIDTH:
-        meteor_x = random.randint(0, WIDTH-METEOR_WIDTH)
-        meteor_y = random.randint(-100, -METEOR_HEIGHT)
-        meteor_speedx = random.randint(-3, 3)
-        meteor_speedy = random.randint(2, 9)
+   # Atualizando a posição dos meteoros
+    meteor1.update()
+    meteor2.update()
+
+  
 
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
     window.blit(background, (0, 0))
-    window.blit(meteor_img_small, (meteor_x, meteor_y))
+
+    window.blit(meteor1.image, meteor1.rect)
+    window.blit(meteor2.image, meteor2.rect)
+
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
