efd7295e065c515273307c4d35bb267de757161b
ceciliaprado <ceciliadmp@al.insper.edu.br>
2021-08-12 11:26:13 -0300

    arrumando

--- assets.py
+++ assets.py
@@ -15,15 +15,17 @@ BOOM_SOUND = 'boom_sound'
 DESTROY_SOUND = 'destroy_sound'
 PEW_SOUND = 'pew_sound'
 
-
 def load_assets():
-    assets = {}
-    assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR, 'starfield.png')).convert()
-    assets[METEOR_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'meteorBrown_med1.png')).convert_alpha()
+    assets= {}
+    assets [BACKGROUND]= pygame.image.load('assets/starfield2.jpg').convert()
+    assets[METEOR_IMG] = pygame.image.load('assets/meteoro2.png').convert_alpha()
     assets[METEOR_IMG] = pygame.transform.scale(assets['meteor_img'], (METEOR_WIDTH, METEOR_HEIGHT))
-    assets[SHIP_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'playerShip1_orange.png')).convert_alpha()
+    assets[BACKGROUND]= pygame.transform.scale(assets['background'], (WIDTH, HEIGHT))
+    assets[SHIP_IMG] = pygame.image.load('assets/nave.png').convert_alpha()
     assets[SHIP_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    assets[BULLET_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
+    assets[BULLET_IMG] = pygame.image.load('assets/laser.png').convert_alpha()
+
+
     explosion_anim = []
     for i in range(9):
         # Os arquivos de animação são numerados de 00 a 08
